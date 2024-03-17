26e4832cc2a9dc7003c276b33f7d270797b3137a
Bryan Herdianto <73747143+bryanherdianto@users.noreply.github.com>
2024-03-17 23:34:26 +0700

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -18,8 +18,8 @@ def xml_to_csv(path):
                      int(root.find('size')[1].text),
                      member[0].text,
                      int(member[5][0].text) if int(member[5][0].text) <= int(root.find('size')[0].text) else int(root.find('size')[0].text),
-                     int(member[5][1].text) if int(member[5][1].text) <= int(root.find('size')[1].text) else int(root.find('size')[1].text),
-                     int(member[5][2].text) if int(member[5][2].text) <= int(root.find('size')[0].text) else int(root.find('size')[0].text),
+                     int(member[5][2].text) if int(member[5][2].text) <= int(root.find('size')[1].text) else int(root.find('size')[1].text),
+                     int(member[5][1].text) if int(member[5][1].text) <= int(root.find('size')[0].text) else int(root.find('size')[0].text),
                      int(member[5][3].text) if int(member[5][3].text) <= int(root.find('size')[1].text) else int(root.find('size')[1].text)
                      )
             xml_list.append(value)
